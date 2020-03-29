c299c9d93d51c7e859b0b82625a462d28850d6cb
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-29 23:59:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,3 @@
 def oxford_comma(array)
-  if array.length == 1 
-    return array.join
-  elsif array.length == 2 
-   return array.join(" and ")
-   elsif array.length == 3
-     return "#{array[0]}, #{array[1]}, and #{array[2]}"
-  elsif array.length > 3
-    temp = []
-    temp << "and #{array[-1]}"
-    array.pop
-    array.push(temp)
-      return array.join(", ")
-  end 
-end 
+
+end
