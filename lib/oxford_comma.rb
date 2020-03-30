266795076df632acff8b75ab463e609700a66358
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-30 00:18:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,11 @@
 def oxford_comma(array)
   if array.length == 1 
-    return array.join
+    array.join
   elsif array.length == 2 
-   return array.join(" and ")
-   elsif array.length == 3
-     return "#{array[0]}, #{array[1]}, and #{array[2]}"
-  elsif array.length > 3
-    temp = []
-    temp << "and #{array[-1]}"
-    array.pop
-    array.push(temp)
-      return array.join(", ")
+    array.join(" and ")
+  elsif array.length >= 3
+    new_array = []
+    new_array << ", and #{array.pop()}"
+    array.join(", ") + new_array.join
   end 
-end 
+end
